03875f7fc4f0e3869093b3e39cbf158f404d272c
Lafaiete do Vale <lafa.vale@gmail.com>
2023-03-22 12:33:08 -0300

    Creating tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 // CSS
 import styles from "./App.module.css";
 
@@ -11,13 +13,19 @@ import TaskList from "./components/TaskList";
 import { ITask } from "./interfaces/Task";
 
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
     <div>
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText="Criar Tarefa" />
+          <TaskForm
+            btnText="Criar Tarefa"
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
         </div>
         <div>
           <h2>Suas tarefas:</h2>
