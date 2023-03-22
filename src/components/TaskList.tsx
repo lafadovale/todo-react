d116183040bf1d069d57f2e8a270a128d0ba91e1
Lafaiete do Vale <lafa.vale@gmail.com>
2023-03-22 14:55:11 -0300

    Task list with action buttons created

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { ITask } from "../interfaces/Task";
 
 // CSS
-import styles from "./Tasklist.module.css";
+import styles from "./TaskList.module.css";
 
 type Props = {
   taskList: ITask[];
@@ -15,12 +15,12 @@ const TaskList = ({ taskList }: Props) => {
     <>
       {taskList.length > 0 ? (
         taskList.map((task) => (
-          <div key={task.id}>
-            <div>
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
               <h4>{task.title}</h4>
               <p>Dificuldade: {task.difficulty}</p>
             </div>
-            <div>
+            <div className={styles.actions}>
               <i className="bi bi-pencil"></i>
               <i className="bi bi-trash"></i>
             </div>
