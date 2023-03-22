58d160d98b1c213ee6a0667be660e2f3d2a79b80
Lafaiete do Vale <lafa.vale@gmail.com>
2023-03-22 00:14:45 -0300

    Header, Footer, Form and initial styles created

---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface Props {
+  btnText: string;
+}
+
+const TaskForm = ({ btnText }: Props) => {
+  return (
+    <form>
+      <div>
+        <label htmlFor="title">Título:</label>
+        <input type="text" name="title" placeholder="Título da tarefa" />
+      </div>
+      <div>
+        <label htmlFor="difficulty">Dificuldade:</label>
+        <input
+          type="text"
+          name="difficulty"
+          placeholder="Dificuldade da tarefa"
+        />
+        <input type="submit" value={btnText} />
+      </div>
+    </form>
+  );
+};
+
+export default TaskForm;
